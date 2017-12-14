3b029939c8be4973d3bf84c487923553ff76157a
c0llision <0xmw@protonmail.com>
2017-12-14 19:55:21 +0000

    use sh

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
+import sh
 import subprocess
+import shlex
 
-subprocess.call(['/usr/bin/git','-C','../hashpass','pull','origin'])
+git = sh.git.bake(_cwd='/Users/marcus/github/hashpass')
+output = git.log('--pretty=%G?',_tty_out=False)
+for line in output:
+    if 'N' in line:
+        print('bad sig!')
 
-with open('f','w') as f:
-    subprocess.call(['/usr/bin/git','-C','../hashpass','log','--pretty=%G?'],stdout=f)
+output = git.tag()
+out=''
+for line in output:
+    thing = line.strip('\n')
+    out = git('verify-tag', 'lol')
+    print(out)
+    
 
-with open('f','r') as f:
-    for line in f:
-        if 'G' in line:
-            print('bad sig!')
+# with open('f','w') as f:
+#     subprocess.call(['/usr/bin/git','-C','../hashpass','tag'],stdout=f)
+
+# with open('f','r') as f:
+#     for line in f:
+#         git = sh.git.bake(_cwd='/Users/marcus/github/hashpass')
+#         try:
+#             print(git.tag('-v', line.strip('\n')))
+#         except sh.ErrorReturnCode_1:
+#             print('INVALID')
