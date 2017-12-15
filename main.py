635b20176ca6c1b862ba2e0a649f01cbfd03b552
c0llision <0xmw@protonmail.com>
2017-12-15 17:55:38 +0000

    init

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
 import sh
-import subprocess
-import shlex
 
-git = sh.git.bake(_cwd='/Users/marcus/github/hashpass')
-output = git.log('--pretty=%G?',_tty_out=False)
-for line in output:
-    if 'N' in line:
-        print('bad sig!')
+ignore_tags = ['bad','lol','lol22']
+allowed_emails =['<0xmw@protonmail.com>']
+git = sh.git.bake(_cwd='/Users/marcus/github/hashpass', _tty_out=False)
 
-output = git.tag()
-out=''
-for line in output:
-    thing = line.strip('\n')
-    out = git('verify-tag', 'lol')
-    print(out)
-    
 
-# with open('f','w') as f:
-#     subprocess.call(['/usr/bin/git','-C','../hashpass','tag'],stdout=f)
+def check_commit_sigs():
+    print('\n'.join(['bad signature ' + line.strip() for line in git.log('--pretty=%G?') if line[0] != 'G']))
 
-# with open('f','r') as f:
-#     for line in f:
-#         git = sh.git.bake(_cwd='/Users/marcus/github/hashpass')
-#         try:
-#             print(git.tag('-v', line.strip('\n')))
-#         except sh.ErrorReturnCode_1:
-#             print('INVALID')
+
+def check_tag_sigs():
+    tags = git.tag().strip().split()
+    for tag in tags:
+        if tag in ignore_tags:
+            continue
+        try:
+            author_email = git.tag('-v', tag, '--format=%(*authoremail)').strip()
+            if author_email in allowed_emails:
+                print('Good tag:', tag, author_email)
+            else:
+                print('Unknown output:', tag, author_email)
+        except Exception as e:
+            if 'error' in str(e):
+                print('Bad tag:', tag)
+                for line in str(e).split('\n'):
+                    if 'error' in line:
+                        print(' ', line)
+            else:
+                raise e
+
+
+def main():
+    # check_commit_sigs(git)
+    check_tag_sigs()
+
+
+main()
